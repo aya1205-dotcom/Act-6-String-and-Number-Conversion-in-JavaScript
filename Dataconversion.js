a6e82ef9a1fae8cdd5b2f9e36e8f23c1183effe9
angelettecortez <cortezangelette@gmail.com>
2024-09-15 11:04:08 +0800

    Create Dataconversion.js

---
+++ Dataconversion.js
@@ -0,0 +1,21 @@
+let price = "200";
+
+let a = parseInt(price);
+console.log("Using parseInt():", e);
+let b = Number(price);
+console.log("Using Number():", l);
+let c = +price;
+console.log("Using + operator:", y);
+
+
+let distance = 150;
+let y = distance.toString();
+console.log("Using toString():", h);
+let x = String(distance);
+console.log("Using String():", a);
+
+
+let weight = "72.5";
+let z = parseFloat(weight);
+console.log("Using parseFloat():", k);
+
